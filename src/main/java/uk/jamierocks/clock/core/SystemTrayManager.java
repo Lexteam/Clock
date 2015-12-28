19adfca8ba39a874ff37e9d73900f4407df64e5c
charlesstewart <tomdr1001@outlook.com>
2015-12-28 15:06:26 +0000

    Implement quit w/ faulty eventbus

--- src/main/java/uk/jamierocks/clock/core/SystemTrayManager.java
+++ src/main/java/uk/jamierocks/clock/core/SystemTrayManager.java
@@ -1,18 +1,55 @@
 package uk.jamierocks.clock.core;
 
 import uk.jamierocks.clock.Main;
+import uk.jamierocks.clock.core.event.tray.TrayEvent;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 
 public class SystemTrayManager {
-    private SystemTray systemTray;
-    private TrayIcon trayIcon;
+    private final SystemTray systemTray;
+    private final TrayIcon trayIcon;
 
     public SystemTrayManager(){
         systemTray = SystemTray.getSystemTray();
         trayIcon = new TrayIcon(createImage("icon.png", "tray icon"));
+
+        PopupMenu menu = new PopupMenu();
+
+        MenuItem alarmsItem = new MenuItem("Alarms");
+        MenuItem visibilityItem = new MenuItem("ToggleVisibility");
+        MenuItem quitItem = new MenuItem("Quit");
+
+        menu.add(alarmsItem);
+        menu.addSeparator();
+        menu.add(visibilityItem);
+        menu.addSeparator();
+        menu.add(quitItem);
+
+        ActionListener listener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                MenuItem item = (MenuItem)e.getSource();
+                if (item.getLabel().equals("Alarms")) {
+                    Main.getInstance().getEventbus().post(new TrayEvent(TrayEvent.TrayEventType.ALARM_OVERVIEW));
+                }
+                else if (item.getLabel().equals("ToggleVisibility")) {
+                    Main.getInstance().getEventbus().post(new TrayEvent(TrayEvent.TrayEventType.TOGGLE_VISABLIITY));
+                }
+                else if (item.getLabel().equals("Quit")) {
+                    Main.getInstance().getEventbus().post(new TrayEvent(TrayEvent.TrayEventType.CLOSE));
+                }
+            }
+        };
+
+        alarmsItem.addActionListener(listener);
+        visibilityItem.addActionListener(listener);
+        quitItem.addActionListener(listener);
+
+        trayIcon.setPopupMenu(menu);
+        trayIcon.setToolTip("Clock");
     }
 
     public void hideIcon() {
